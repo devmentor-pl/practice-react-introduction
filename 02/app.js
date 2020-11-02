615900efb49e9c633fc9f426afc88769529d15a3
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-11-02 16:13:11 +0100

    Finish task 2

---
+++ 02/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const HeaderFn = () => <header>Moja pierwsza strona w React</header>;
+
+class HeaderCl extends Component {
+  render() {
+    return <header>Moja pierwsza strona w React</header>;
+  }
+}
+
+ReactDOM.render(<HeaderCl />, document.getElementById("root"));
