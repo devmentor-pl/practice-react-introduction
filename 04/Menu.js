842b7e69ae4b8b3159d6a1babda10417b8d6f0c3
AnnaBuller <aniabela@wp.pl>
2020-11-19 17:11:26 +0100

    add li from arr generating

--- 04/Menu.js
+++ 04/Menu.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import MenuItem from '../03/MenuItem'
 
-const Menu = () => {
+const Menu = (props) => {
+  const itemsData = props.items;
+  const liItems = itemsData.map(item => {
+    const { text, url } = item
+    return <MenuItem key={text} text={text} url={url} />
+  })
   return (
     <ul>
-      <MenuItem text="home" url="/" />
-      <MenuItem text="kontakt" url="/contact" />
-    </ul>)
+      {liItems}
+    </ul>
+  )
 }
 
 export default Menu
