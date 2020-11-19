b32737d91dccc3b14e7d80f1026f58568344a60c
AnnaBuller <aniabela@wp.pl>
2020-11-19 16:23:03 +0100

    import removed nav

--- 04/app.js
+++ 04/app.js
@@ -1,20 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MenuItem from '../03/MenuItem'
-
-const Menu = () => {
-  return (
-    <ul>
-      <MenuItem text="home" url="/" />
-      <MenuItem text="kontakt" url="/contact" />
-    </ul>)
-}
-
-const Nav = () => {
-  return (
-    <nav>
-      <Menu />
-    </nav>)
-}
+import Nav from './Nav'
 
 ReactDOM.render(<Nav />, document.querySelector('#root'))
