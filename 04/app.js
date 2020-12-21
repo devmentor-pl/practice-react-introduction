885bf0d89259de0e5fbfe1ca07bef137f6aba0f0
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-12-21 16:53:09 +0100

    Task 04 finished

---
+++ 04/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MenuItem from '../03/components/MenuItem';
+
+const Menu = () => {
+  return (
+    <ul>
+      <MenuItem text='home' url='/' />
+      <MenuItem text='kontakt' url='/contact' />
+    </ul>
+  );
+};
+
+const Nav = () => {
+  return (
+    <nav>
+      <Menu />
+    </nav>
+  );
+};
+
+ReactDOM.render(<Nav />, document.querySelector('#root'));
