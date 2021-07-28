7d44e26c3c71e7660e1b314bfb9f98fe73263d39
Laura Zacharuk <laura.zacharuk@gmail.com>
2021-07-28 14:39:04 +0200

    done task 3

--- 03/app.js
+++ 03/app.js
@@ -3,3 +3,22 @@
 // Ten komponent przez props przyjmuje wartości jakie ma renderować tj. <MenuItem text="kontakt" url="/contact"/>.
 
 // Cały komponent zapisz w osobny pliku np. MenuItem.js oraz użyj importu i eksportu, aby go uruchomić w pliku app.js.
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MenuItem from './MenuItem.js';
+
+const App = () => {
+    return (
+    <ul>
+    <MenuItem text='kontakt' url='/contact' />
+</ul>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
