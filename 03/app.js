36629298ae97cee7a8d6b8ae3235c42911b935cb
Laura Zacharuk <laura.zacharuk@gmail.com>
2021-07-28 13:08:46 +0200

    done task 1

---
+++ 03/app.js
@@ -0,0 +1,5 @@
+// Twoje zadanie polega na utworzenu komponentu <MenuItem/>, który ma renderować element menu tj. <li/> zawierające element <a/>;
+
+// Ten komponent przez props przyjmuje wartości jakie ma renderować tj. <MenuItem text="kontakt" url="/contact"/>.
+
+// Cały komponent zapisz w osobny pliku np. MenuItem.js oraz użyj importu i eksportu, aby go uruchomić w pliku app.js.
