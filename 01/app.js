bb43ec9ff128bb49ec5b44d244d5c0202f495a6b
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-18 13:41:57 +0100

    01 solved

--- 01/app.js
+++ 01/app.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>React działa!</h1>), document.querySelector("#root");
+ReactDOM.render(<h1>React działa!</h1>, document.querySelector("#root"));
