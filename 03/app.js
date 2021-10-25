e9ce87e138b7d6a9423d3c6c32e6ec61f93b4ecc
Jakub Chrobak <jchrobakk@gmail.com>
2021-10-25 14:41:07 +0200

    Finish task 03

---
+++ 03/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MenuItem from "./components/MenuItem";
+
+ReactDOM.render(
+  <MenuItem text="Kontakt" url="/contact" />,
+  document.querySelector("#root")
+);
