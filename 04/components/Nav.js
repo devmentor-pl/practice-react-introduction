37ef3f3fa88e87c21ae4573add0c4c0c24585f97
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-11-21 18:16:02 +0100

    Task 04 - done

---
+++ 04/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Menu from './Menu'
+
+export default class Nav extends React.Component {
+    render() {
+        return (
+            <nav>
+                <Menu />
+            </nav>
+        )
+    }
+}
