711305001cda08677b9681ab2d4c1df962406f64
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-11-30 20:40:07 +0100

    Fourth task

---
+++ 04/components/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MenuItems from "../../03/components/MenuItems"
+
+const Menu = () => 
+    <>
+            <MenuItems text="home" url="/" />
+            <MenuItems text="kontakt" url="/contact" />
+    </>
+
+
+export default Menu;
