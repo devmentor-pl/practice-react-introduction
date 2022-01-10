5367637afb749bd2f52f51e681e628a3fe62f428
Rafał Kazik <kazik.raf@gmail.com>
2022-01-10 23:31:45 +0100

    5th task

---
+++ 05/components/Menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+
+const Menu = () => {
+  return (
+    <ul>
+      <MenuItem text="home" url="/" />
+      <MenuItem text="kontakt" url="/contact" />
+    </ul>
+  );
+};
+
+export default Menu;
