94158d4820d2901562c4776c74d3971916ac5cdd
Rafał Kazik <kazik.raf@gmail.com>
2022-01-14 21:20:08 +0100

    adding props

--- 05/components/Menu.js
+++ 05/components/Menu.js
@@ -2,17 +2,15 @@ import React from "react";
 // import MenuItem from "./MenuItem";
 import PropTypes from "prop-types";
 
-const Menu = () => {
-  const arr = [
-    { text: "strona główna", url: "/" },
-    { text: "www", url: "/www" },
-  ];
+const Menu = (props) => {
+  const menuArr = props.menuArr;
 
-  const arrMap = arr.map((item, index) => (
+  const arrMap = menuArr.map((item, index) => (
     <li key={index}>
       <a href={item.url}>{item.text}</a>
     </li>
   ));
+
   const ulElement = <ul>{arrMap}</ul>;
 
   return <React.Fragment>{ulElement}</React.Fragment>;
