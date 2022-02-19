de9168c9dccc1bd149297c07bdbe1f57ed64738c
Ewelina <ewelina@ekopacz.pl>
2022-02-20 00:17:57 +0100

    finish task 03 - remove ul tag from MenuItem

--- 03/components/MenuItem.js
+++ 03/components/MenuItem.js
@@ -3,11 +3,9 @@ import React from 'react';
 class MenuItem extends React.Component{
     render(){
         return(
-            <ul>
-                <li>
-                    <a href={this.props.url}>{this.props.text}</a>
-                </li>
-            </ul>
+            <li>
+                <a href={this.props.url}>{this.props.text}</a>
+            </li>
         )
     }
 }
