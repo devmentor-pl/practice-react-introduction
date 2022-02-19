f948985626a83c43c90debb4b349dac84c2a9169
Ewelina <ewelina@ekopacz.pl>
2022-02-20 00:17:21 +0100

    finish task 04

---
+++ 04/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MenuItem from '../03/components/MenuItem';
+
+
+const Menu = () => (
+    <ul>
+        <MenuItem text="home" url="/"/>
+        <MenuItem text="kontakt" url="/contact"/>
+    </ul>
+)
+const Nav = () => <nav><Menu/></nav>
+
+ReactDOM.render(<Nav/>,document.querySelector("#root"));
+
