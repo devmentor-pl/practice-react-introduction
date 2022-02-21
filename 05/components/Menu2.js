2705108a82806d52a89ff4bafc59072ca0d610bf
Ewelina <ewelina@ekopacz.pl>
2022-02-21 21:00:31 +0100

    added method in class

--- 05/components/Menu2.js
+++ 05/components/Menu2.js
@@ -3,17 +3,18 @@ import React from 'react';
 class Menu2 extends React.Component{
     render(){
         return (
-            <ul>
-            {this.props.items.map((item) => {
-                return (
-                    <li>
-                        <a href ={item.url}>{item.text}</a>
-                    </li>
-                    )
-                })
-            }
-            </ul>
+            <ul>{this.renderItems()}</ul>
         )
+    }
+    renderItems(){
+        {this.props.items.map((item) => {
+            return (
+                <li>
+                    <a href ={item.url}>{item.text}</a>
+                </li>
+                )
+            })
+        }
     }
 }
 
