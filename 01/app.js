30b5c7a5a34590de7243731d08c3c6228f68ff32
Andrzej <andis_as@wp.pl>
2022-06-27 09:19:08 +0200

    zad 1 - done

---
+++ 01/app.js
@@ -0,0 +1,16 @@
+console.log('zad 01')
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = () => <h1>React działa!</h1>
+
+ReactDOM.render(
+    <Header />,
+    document.querySelector('#root')
+)
+
+
+
+
+
