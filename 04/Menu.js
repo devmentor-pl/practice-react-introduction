2b711a0916387724135aca1980a147cc51a3c588
Mateusz Suplewski <matx3582@gmail.com>
2022-08-02 19:36:02 +0200

    Task-4 Done

---
+++ 04/Menu.js
@@ -0,0 +1,13 @@
+import React from "react"
+import MenuItem from "../03/MenuItem"
+
+const Menu = () => {
+    return (
+        <ul>
+            <MenuItem text="home" url="/" />
+            <MenuItem text="kontakt" url="/contact" />
+        </ul>
+    )
+}
+
+export default Menu
