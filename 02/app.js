875d7db1102eedafb464d39887f789f18d15490a
Mateusz Suplewski <matx3582@gmail.com>
2022-08-02 19:15:33 +0200

    Task-2 Styles added

--- 02/app.js
+++ 02/app.js
@@ -3,15 +3,24 @@ import ReactDOM from "react-dom"
 
 const HeaderFn = () => {
     return (
-        <header>
+        <header style={{ border: '1px solid gray', color: 'pink', borderRadius: '4px', width: '50%', textAlign: "center" }}>
             {'Moja pierwsza strona w React'}
         </header>
     )
 }
 
+const clHeaderStyles = {
+    textAlign: "center",
+    width: '50%',
+    color: 'white',
+    border: '1px solid gray',
+    backgroundColor: 'black',
+    borderRadius: '4px'
+}
+
 class HeaderCl extends React.Component {
     render() {
-        return <header>{'Moja pierwsza strona w React'}</header>
+        return <header style={clHeaderStyles}>{'Moja pierwsza strona w React'}</header>
     }
 }
 
