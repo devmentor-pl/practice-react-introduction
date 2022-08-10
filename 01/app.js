46b353ea42578112cd0f9f04ca339458784c1857
Dawid Drag <dede7d@wp.pl>
2022-08-10 16:39:13 +0200

    task 01 done

---
+++ 01/app.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+const h1Element = (
+    <h1>React działa!!!</h1>
+)
+
+
+ReactDOM.render(
+    h1Element,
+    document.querySelector('#root')
+)
