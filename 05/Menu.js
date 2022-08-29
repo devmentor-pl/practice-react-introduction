501cc3185db2b93abcee2bfd586bb227f55756eb
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-08-29 11:56:32 +0200

    finish additional exercise

--- 05/Menu.js
+++ 05/Menu.js
@@ -4,11 +4,27 @@ import ReactDOM from 'react-dom';
 
 import MenuItem from '../03/MenuItem';
 
-const Menu = () => {
-	const arr = [<MenuItem text='strona główna' url='/' />, <MenuItem text='www' url='/www' />];
-	const jsx = <ul>{arr}</ul>;
-	return jsx;
-};
+// const Menu = () => {
+// 	const arr = [<MenuItem text='strona główna' url='/' />, <MenuItem text='www' url='/www' />];
+// 	const jsx = <ul>{arr}</ul>;
+// 	return jsx;
+// };
+
+const Menu = (props) => {
+	const { items } = props;
+	return (
+		<ul>
+			{
+				items.map(
+					item => {
+						const { text, url } = item;
+						return <MenuItem text={text} url={url} />
+					}
+				)
+			}
+		</ul>
+	)
+}
 
 MenuItem.PropTypes = {
 	text: PropTypes.string.isRequired,
