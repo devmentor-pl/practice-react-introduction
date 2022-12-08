fb42a7f0ed4a22027a593a7b1e6e8169696b3586
Jacek Zielinski <gandi34@wp.pl>
2022-12-08 09:06:32 +0100

    02 almost done

--- 02/app.js
+++ 02/app.js
@@ -1,4 +1,15 @@
-// Rozwiązanie przy wykorzystaniu funkcji
+//Style
+
+const functionStyle = {
+    border: '3px solid black',
+    backgroundColor: 'yellow',
+    color:'red',
+    borderradius: '5px',
+};
+
+
+
+//Rozwiązanie przy wykorzystaniu funkcji
 
 import React from 'react';
 import {createRoot} from 'react-dom/client';
@@ -8,7 +19,41 @@ const root = createRoot(document.querySelector('#root'));
 const getText = () => 'Moja pierwsza strona w React !';
 
 root.render(
-    <header>
+    <headerFn
+        style ={functionStyle}
+    >
         {getText()}
-    </header>
+    </headerFn>
 );
+
+
+
+
+
+//Rozwiązanie z klasami ES2015
+
+/*import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+const root = createRoot(document.querySelector('#root'));
+ 
+class Text {
+    constructor() {
+        this.words = 'Moja pierwsza strona w React!';
+    }
+} 
+
+const newText = new Text();
+
+root.render(
+    <header>
+       
+    </header>
+
+)
+*/
+    
+
+
+
+
