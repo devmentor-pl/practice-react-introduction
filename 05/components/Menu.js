5fe2142c15a7b5e3da66ec2733928467576e9942
zimiier <matmatwiej@gmail.com>
2023-02-20 12:11:19 +0100

    Finish task 5

---
+++ 05/components/Menu.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import MenuItem from './MenuItem';
+
+class Menu extends React.Component {
+	render() {
+		const { items } = this.props;
+		const menuList = items.map((item) => (
+			<MenuItem url={item.url} text={item.text} />
+		));
+
+		return <ul>{menuList}</ul>;
+	}
+}
+
+export default Menu;
---
+++ 05/components/Nav.js
@@ -0,0 +1,19 @@
+﻿import React from 'react';
+import Menu from './Menu';
+
+class Nav extends React.Component {
+	render() {
+		return (
+			<nav>
+				<Menu
+					items={[
+						{ url: '/', text: 'home' },
+						{ url: '/contact', text: 'kontakt' },
+					]}
+				/>
+			</nav>
+		);
+	}
+}
+
+export default Nav;
