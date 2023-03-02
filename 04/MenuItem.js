409121a062f28805aa1a72e8b9fbace0757a8fcf
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2023-03-02 18:52:10 +0100

    04 done

---
+++ 04/MenuItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const MenuItem = (props) => {
+    return (
+        <li>
+            <a href={props.url}>{props.text}</a>
+        </li>
+    );
+}
+export default MenuItem;
