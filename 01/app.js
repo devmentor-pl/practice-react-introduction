06d54edcf2381c3006a3a62cb8f0c3a64bbf755b
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-04-12 11:05:26 +0200

    Add ex 1

---
+++ 01/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.querySelector("#root"));
+const text = "React działa";
+
+root.render(<h1>{text}</h1>);
