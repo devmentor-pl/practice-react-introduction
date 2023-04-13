ae8cd51988af230d67d0c06f936e76ba0656cf9b
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-04-13 21:31:56 +0200

    task-05 done

---
+++ 05/components/MenuItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MenuItem extends React.Component {
+	render() {
+		return (
+			<li>
+				<a href={this.props.url}> {this.props.text} </a>
+			</li>
+		);
+	}
+}
+
+MenuItem.propTypes = {
+	url: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
+};
+
+export default MenuItem;
