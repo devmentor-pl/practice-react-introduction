55670059ee74cae5b780efb51952665bb6ed8445
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-05-29 20:32:29 +0200

    finish task 2

---
+++ 02/components/HeaderFn.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HeaderFn = () => {
+    return (
+        <header
+            style= {{
+                textShadow: "0 0 3px #FF0000, 0 0 5px #0000FF",
+                fontSize: "40px"
+        }}
+        >
+            {'Moja pierwsza strona w React'}
+        </header>
+    );
+}
+export default HeaderFn;
