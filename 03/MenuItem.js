abe20a249e99d1db333ee8903893e20fd1ff08b5
Marcin Soltysik <profittax@gmail.com>
2023-08-27 18:11:39 +0200

    FIX: React-e1-done 03

--- 03/MenuItem.js
+++ 03/MenuItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
+
 const MenuItem = function (props) {
+  const { text, url } = props;
   return (
     <ul>
       <li>
-        <a>{props.text}</a>
-      </li>
-      <li>
-        <a>{props.url}</a>
+        <a href={url} target="blank">
+          {text}
+        </a>
       </li>
     </ul>
   );
--- 03/app.js
+++ 03/app.js
@@ -4,7 +4,7 @@ import MenuItem from './MenuItem';
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 const App = function () {
-  return <MenuItem text="kontakt" url="/contact" />;
+  return <MenuItem text="onet" url="https://www.onet.pl/" />;
 };
 
 root.render(<App />);
