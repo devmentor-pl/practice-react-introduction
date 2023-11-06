f139782a3912acc1330fff1cba183d4b320217e2
Maciej Necka <macieknecka@gmail.com>
2023-11-06 16:01:49 +0100

    Task done

--- 03/MenuItem.js
+++ 03/MenuItem.js
@@ -3,11 +3,9 @@ import React from 'react';
 const MenuItem = (props) => {
   const { text, url } = props;
   return (
-    <ul>
-      <li>
-        <a url={url}>{text}</a>
-      </li>
-    </ul>
+    <li>
+      <a url={url}>{text}</a>
+    </li>
   );
 };
 
