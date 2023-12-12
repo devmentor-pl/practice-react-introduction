716f8d6900fce335c92f65752c35475284f03956
Kaczupinko <kaczupinko@gmail.com>
2023-12-12 08:03:22 +0000

    Fix MenuItem class

--- 03/src/components/MenuItem.js
+++ 03/src/components/MenuItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class MenuItem extends React.Component {
     render() {
-        const { text, url } = this.props.item;
+        const { text, url } = this.props;
         return (
             <li>
                 <a href={url}>{text}</a>
