30a8aa86dc2f970709d4425b1ca622b923caabd7
Kasia <k.kasia912@gmail.com>
2024-01-19 14:12:27 +0100

    webpack config problems

--- 01/app.js
+++ 01/app.js
@@ -1,5 +1,5 @@
-import React from './react';
-import {createRoot} from './react-dom/client';
+import React from 'react';
+import {createRoot} from 'react-dom/client';
 
 const root = createRoot(document.querySelector('#root'));
 const text = 'React działa!';
