a4b38ef1f0a109b0b127cc437daa9f45ca1a1bbb
Kasia <k.kasia912@gmail.com>
2024-01-19 13:58:00 +0100

    add webpack config

---
+++ 01/app.js
@@ -0,0 +1,14 @@
+import React from './react';
+import {createRoot} from './react-dom/client';
+
+const root = createRoot(document.querySelector('#root'));
+const text = 'React działa!';
+const H1 = () => {
+  return (
+    <>
+      <h1>{text}</h1>
+    </>
+  );
+};
+
+root.render(<H1 />);
