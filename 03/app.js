913829bd3c28c86571dc700530245371716ed69c
Dawidtk <dawidtk@gmail.com>
2024-01-31 09:36:21 +0100

    done

---
+++ 03/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MenuItem from './MenuItem';
+
+ReactDOM.render(
+    <MenuItem text="My Linkedin" url="https://www.linkedin.com/in/davidtk/" />,
+    document.querySelector('#root')
+);
